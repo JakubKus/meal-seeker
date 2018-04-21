47625247723fc3137f956e619ac9437898607b71
Kuba <kuba.mistrz@o2.pl>
2018-04-21 23:41:03 +0200

    splitted questions and meals into divs...
    * changed colors
    * changed div.container into main.container

--- src/index.js
+++ src/index.js
@@ -15,8 +15,8 @@ class Buttons extends React.Component {
     let buttons = document.getElementsByTagName("BUTTON");
     this.setState({changeButtonColor: "rgb(170, 170, 170)"});
     for(let button of buttons) {
-      if(button.style.color === "rgb(0, 255, 0)") {
-        this.setState({changeButtonColor: "rgb(0, 0, 0)"})
+      if(button.style.color === "rgb(0, 200, 0)") {
+        this.setState({changeButtonColor: "rgb(25, 186, 0)"})
       }
     }
   };
@@ -57,156 +57,160 @@ class RenderButtons extends React.Component {
   }
 
   changeButtonsColor = (e) => {
-    e.target.style.color = (e.target.style.color === "rgb(0, 255, 0)") ? "rgb(0, 0, 0)" : "rgb(0, 255, 0)";
+    e.target.style.color = (e.target.style.color === "rgb(0, 200, 0)") ? "rgb(0, 0, 0)" : "rgb(0, 200, 0)";
     let answer = e.target.innerHTML.trim();
     this.setState((this.state[answer] === false) ? {[answer]: true} : {[answer]: false});
   };
 
   render() {
     return(
-      <div className="container">
+      <main className="container">
         <h2 className="PageTitle">Food Picker</h2>
-        <p>Calories / 100g</p>
-        <button onClick={(e) => { this.changeButtonsColor(e); this.props.activate()} } > Less than 200 </button>
-        <button onClick={(e) => { this.changeButtonsColor(e); this.props.activate()} } > 200 - 500 </button>
-        <button onClick={(e) => { this.changeButtonsColor(e); this.props.activate()} } > More than 500 </button>
-        <p>Flavor</p>
-        <button onClick={(e) => { this.changeButtonsColor(e); this.props.activate()} } > Bitter </button>
-        <button onClick={(e) => { this.changeButtonsColor(e); this.props.activate()} } > Salty </button>
-        <button onClick={(e) => { this.changeButtonsColor(e); this.props.activate()} } > Sour </button>
-        <button onClick={(e) => { this.changeButtonsColor(e); this.props.activate()} } > Spicy </button>
-        <button onClick={(e) => { this.changeButtonsColor(e); this.props.activate()} } > Sweet </button>
-        <p>Preparation time [mins]</p>
-        <button onClick={(e) => { this.changeButtonsColor(e); this.props.activate()} } > Less than 5</button>
-        <button onClick={(e) => { this.changeButtonsColor(e); this.props.activate()} } > 5 - 15 </button>
-        <button onClick={(e) => { this.changeButtonsColor(e); this.props.activate()} } > More than 15 </button>
-        <p>Difficulty level</p>
-        <button onClick={(e) => { this.changeButtonsColor(e); this.props.activate()} } > Easy </button>
-        <button onClick={(e) => { this.changeButtonsColor(e); this.props.activate()} } > Medium </button>
-        <button onClick={(e) => { this.changeButtonsColor(e); this.props.activate()} } > Hard </button>
-        <button onClick={(e) => { this.changeButtonsColor(e); this.props.activate()} } > Very hard </button>
-        <p>Temperature</p>
-        <button onClick={(e) => { this.changeButtonsColor(e); this.props.activate()} } > Cold </button>
-        <button onClick={(e) => { this.changeButtonsColor(e); this.props.activate()} } > Hot </button>
-        <p>Macros</p>
-        <button onClick={(e) => { this.changeButtonsColor(e); this.props.activate()} } > High carb </button>
-        <button onClick={(e) => { this.changeButtonsColor(e); this.props.activate()} } > High fat </button>
-        <button onClick={(e) => { this.changeButtonsColor(e); this.props.activate()} } > High protein </button>
-        <button style={{color: this.props.changeButton}}>Choose</button>
+        <div className="questions">
+          <p>Calories / 100g</p>
+          <button onClick={(e) => { this.changeButtonsColor(e); this.props.activate()} } > Less than 200 </button>
+          <button onClick={(e) => { this.changeButtonsColor(e); this.props.activate()} } > 200 - 500 </button>
+          <button onClick={(e) => { this.changeButtonsColor(e); this.props.activate()} } > More than 500 </button>
+          <p>Flavor</p>
+          <button onClick={(e) => { this.changeButtonsColor(e); this.props.activate()} } > Bitter </button>
+          <button onClick={(e) => { this.changeButtonsColor(e); this.props.activate()} } > Salty </button>
+          <button onClick={(e) => { this.changeButtonsColor(e); this.props.activate()} } > Sour </button>
+          <button onClick={(e) => { this.changeButtonsColor(e); this.props.activate()} } > Spicy </button>
+          <button onClick={(e) => { this.changeButtonsColor(e); this.props.activate()} } > Sweet </button>
+          <p>Preparation time [mins]</p>
+          <button onClick={(e) => { this.changeButtonsColor(e); this.props.activate()} } > Less than 5</button>
+          <button onClick={(e) => { this.changeButtonsColor(e); this.props.activate()} } > 5 - 15 </button>
+          <button onClick={(e) => { this.changeButtonsColor(e); this.props.activate()} } > More than 15 </button>
+          <p>Difficulty level</p>
+          <button onClick={(e) => { this.changeButtonsColor(e); this.props.activate()} } > Easy </button>
+          <button onClick={(e) => { this.changeButtonsColor(e); this.props.activate()} } > Medium </button>
+          <button onClick={(e) => { this.changeButtonsColor(e); this.props.activate()} } > Hard </button>
+          <button onClick={(e) => { this.changeButtonsColor(e); this.props.activate()} } > Very hard </button>
+          <p>Temperature</p>
+          <button onClick={(e) => { this.changeButtonsColor(e); this.props.activate()} } > Cold </button>
+          <button onClick={(e) => { this.changeButtonsColor(e); this.props.activate()} } > Hot </button>
+          <p>Macros</p>
+          <button onClick={(e) => { this.changeButtonsColor(e); this.props.activate()} } > High carb </button>
+          <button onClick={(e) => { this.changeButtonsColor(e); this.props.activate()} } > High fat </button>
+          <button onClick={(e) => { this.changeButtonsColor(e); this.props.activate()} } > High protein </button>
+        </div>
+        <div className="meals">
+          <p style=
+               {{ display:
+                   ((
+                       this.state["200 - 500"] ||
+                       this.state["Sweet"] ||
+                       this.state["Less than 5"] ||
+                       this.state["Easy"] ||
+                       this.state["Cold"] ||
+                       this.state["High carb"]
+                     ) &&
+                     !this.state["Less than 200"] &&
+                     !this.state["More than 500"] &&
+                     !this.state["Salty"] &&
+                     !this.state["Bitter"] &&
+                     !this.state["Sour"] &&
+                     !this.state["Spicy"] &&
+                     !this.state["5 - 15"] &&
+                     !this.state["More than 15"] &&
+                     !this.state["Medium"] &&
+                     !this.state["Hard"] &&
+                     !this.state["Very hard"] &&
+                     !this.state["Hot"] &&
+                     !this.state["High fat"] &&
+                     !this.state["High protein"]
+                   ) ? "block" : "none"
+               }}>Breakfast cereals
+          </p>
 
-        <p style=
-          {{ display:
-            ((
-              this.state["200 - 500"] ||
-              this.state["Sweet"] ||
-              this.state["Less than 5"] ||
-              this.state["Easy"] ||
-              this.state["Cold"] ||
-              this.state["High carb"]
-              ) &&
-              !this.state["Less than 200"] &&
-              !this.state["More than 500"] &&
-              !this.state["Salty"] &&
-              !this.state["Bitter"] &&
-              !this.state["Sour"] &&
-              !this.state["Spicy"] &&
-              !this.state["5 - 15"] &&
-              !this.state["More than 15"] &&
-              !this.state["Medium"] &&
-              !this.state["Hard"] &&
-              !this.state["Very hard"] &&
-              !this.state["Hot"] &&
-              !this.state["High fat"] &&
-              !this.state["High protein"]
-            ) ? "block" : "none"
-          }}>Breakfast cereals
-        </p>
+          <p style=
+               {{ display:
+                   ((
+                       this.state["200 - 500"] ||
+                       this.state["Sweet"] ||
+                       this.state["More than 15"] ||
+                       this.state["Medium"] ||
+                       this.state["Cold"] ||
+                       this.state["High carb"] ||
+                       this.state["High fat"]
+                     ) &&
+                     !this.state["Less than 200"] &&
+                     !this.state["More than 500"] &&
+                     !this.state["Salty"] &&
+                     !this.state["Bitter"] &&
+                     !this.state["Sour"] &&
+                     !this.state["Spicy"] &&
+                     !this.state["Less than 5"] &&
+                     !this.state["5 - 15"] &&
+                     !this.state["Easy"] &&
+                     !this.state["Hard"] &&
+                     !this.state["Very hard"] &&
+                     !this.state["Hot"] &&
+                     !this.state["High protein"]
+                   ) ? "block" : "none"
+               }}>Cheesecake
+          </p>
 
-        <p style=
-          {{ display:
-            ((
-              this.state["200 - 500"] ||
-              this.state["Sweet"] ||
-              this.state["More than 15"] ||
-              this.state["Medium"] ||
-              this.state["Cold"] ||
-              this.state["High carb"] ||
-              this.state["High fat"]
-              ) &&
-              !this.state["Less than 200"] &&
-              !this.state["More than 500"] &&
-              !this.state["Salty"] &&
-              !this.state["Bitter"] &&
-              !this.state["Sour"] &&
-              !this.state["Spicy"] &&
-              !this.state["Less than 5"] &&
-              !this.state["5 - 15"] &&
-              !this.state["Easy"] &&
-              !this.state["Hard"] &&
-              !this.state["Very hard"] &&
-              !this.state["Hot"] &&
-              !this.state["High protein"]
-            ) ? "block" : "none"
-          }}>Cheesecake
-        </p>
+          <p style=
+               {{ display:
+                   ((
+                       this.state["200 - 500"] ||
+                       this.state["Sweet"] ||
+                       this.state["5 - 15"] ||
+                       this.state["Medium"] ||
+                       this.state["Hot"] ||
+                       this.state["High carb"] ||
+                       this.state["High fat"]
+                     ) &&
+                     !this.state["Less than 200"] &&
+                     !this.state["More than 500"] &&
+                     !this.state["Bitter"] &&
+                     !this.state["Salty"] &&
+                     !this.state["Sour"] &&
+                     !this.state["Spicy"] &&
+                     !this.state["Less than 5"] &&
+                     !this.state["More than 15"] &&
+                     !this.state["Easy"] &&
+                     !this.state["Hard"] &&
+                     !this.state["Very hard"] &&
+                     !this.state["Cold"] &&
+                     !this.state["High protein"]
+                   ) ? "block" : "none"
+               }}>Pancakes
+          </p>
 
-        <p style=
-          {{ display:
-            ((
-              this.state["200 - 500"] ||
-              this.state["Sweet"] ||
-              this.state["5 - 15"] ||
-              this.state["Medium"] ||
-              this.state["Hot"] ||
-              this.state["High carb"] ||
-              this.state["High fat"]
-              ) &&
-              !this.state["Less than 200"] &&
-              !this.state["More than 500"] &&
-              !this.state["Bitter"] &&
-              !this.state["Salty"] &&
-              !this.state["Sour"] &&
-              !this.state["Spicy"] &&
-              !this.state["Less than 5"] &&
-              !this.state["More than 15"] &&
-              !this.state["Easy"] &&
-              !this.state["Hard"] &&
-              !this.state["Very hard"] &&
-              !this.state["Cold"] &&
-              !this.state["High protein"]
-            ) ? "block" : "none"
-          }}>Pancakes
-        </p>
-
-        <p style=
-          {{ display:
-            ((
-              this.state["Less than 200"] ||
-              this.state["Salty"] ||
-              this.state["More than 15"] ||
-              this.state["Medium"] ||
-              this.state["Hot"] ||
-              this.state["High carb"]
-              ) &&
-              !this.state["200 - 500"] &&
-              !this.state["More than 500"] &&
-              !this.state["Bitter"] &&
-              !this.state["Sour"] &&
-              !this.state["Spicy"] &&
-              !this.state["Sweet"] &&
-              !this.state["Less than 5"] &&
-              !this.state["5 - 15"] &&
-              !this.state["Easy"] &&
-              !this.state["Hard"] &&
-              !this.state["Very hard"] &&
-              !this.state["Cold"] &&
-              !this.state["High fat"] &&
-              !this.state["High protein"]
-            ) ? "block" : "none"
-          }}>Spaghetti
-        </p>
-
-      </div>
+          <p style=
+               {{ display:
+                   ((
+                       this.state["Less than 200"] ||
+                       this.state["Salty"] ||
+                       this.state["More than 15"] ||
+                       this.state["Medium"] ||
+                       this.state["Hot"] ||
+                       this.state["High carb"]
+                     ) &&
+                     !this.state["200 - 500"] &&
+                     !this.state["More than 500"] &&
+                     !this.state["Bitter"] &&
+                     !this.state["Sour"] &&
+                     !this.state["Spicy"] &&
+                     !this.state["Sweet"] &&
+                     !this.state["Less than 5"] &&
+                     !this.state["5 - 15"] &&
+                     !this.state["Easy"] &&
+                     !this.state["Hard"] &&
+                     !this.state["Very hard"] &&
+                     !this.state["Cold"] &&
+                     !this.state["High fat"] &&
+                     !this.state["High protein"]
+                   ) ? "block" : "none"
+               }}>Spaghetti
+          </p>
+        </div>
+        <div className="chooseButton" style={{backgroundColor: this.props.changeButton}}>
+          <button style={{color: this.props.changeButton}}>Choose</button>
+        </div>
+      </main>
     )
   }
 }
