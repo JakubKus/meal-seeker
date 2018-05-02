90fc96f8fe80e5103f1065d177bdd0bcff0944fe
Kuba <kuba.mistrz@o2.pl>
2018-05-02 14:43:24 +0200

    installed jest with enzyme [tests]...
    * added required dependencies for jest and enzyme to package.json and installed it
    * prepared directory and one file to write tests

---
+++ tests/index.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Enzyme, {shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({adapter: new Adapter()});
+
+import SingleButton from '../src/singleButton';
+import Body from '../src/body';
