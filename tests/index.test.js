0823de3b1c871c5b4c6e8dd4b76af5f501166487
Kuba <kuba.mistrz@o2.pl>
2018-05-06 12:48:25 +0200

    deleted index.js test
    * there was nothing to test

--- tests/index.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Enzyme, {shallow} from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
-Enzyme.configure({adapter: new Adapter()});
-
-import SingleButton from '../src/singleButton';
-import Body from '../src/body';
