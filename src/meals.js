b6e7ae36c39da5913dd489c71e51a383ad5bcac2
Kuba <kubakus97@o2.pl>
2018-07-12 11:17:02 +0200

    put meals into collapse components and styled them

--- src/meals.js
+++ src/meals.js
@@ -1,14 +1,15 @@
 import React from "react";
+import { Collapse } from 'reactstrap';
 
 export default class Meals extends React.Component {
   render() {
     const meals = this.props.meals.map((element, index) => {
       return (
-        <p key={index}
-           className={element.isActive ? "" : "inactive"}
+        <Collapse key={index}
+          isOpen={element.isActive}
         >
-          {element.name}
-        </p>
+          <p>{element.name}</p>
+        </Collapse>
       );
     });
     return <aside className="meals">{meals}<div className="filler"/></aside>
