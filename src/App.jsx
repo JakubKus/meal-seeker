e46e938af1ea65fe899733b78f43af177bee0e9f
Kuba <kuba.mistrz@o2.pl>
2018-11-25 00:11:11 +0100

    renamed Body component and refactored code

---
+++ src/App.jsx
@@ -0,0 +1,425 @@
+import React, { Component } from 'react';
+import Form from './form';
+import Meals from './meals';
+import Footer from './footer';
+import RandomMealPopup from './randomMealPopup';
+import Blur from './blur';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      form: [
+        { type: 'question', content: 'Preparation time [mins]' },
+        { type: 'answer', content: 'Less than 10', isChecked: false },
+        { type: 'answer', content: '10 - 20', isChecked: false },
+        { type: 'answer', content: 'More than 20', isChecked: false },
+        { type: 'question', content: 'Temperature' },
+        { type: 'answer', content: 'Hot', isChecked: false },
+        { type: 'answer', content: 'Cold', isChecked: false },
+        { type: 'question', content: 'Flavor' },
+        { type: 'answer', content: 'Salty', isChecked: false },
+        { type: 'answer', content: 'Sweet', isChecked: false },
+        { type: 'answer', content: 'Spicy', isChecked: false },
+        { type: 'question', content: 'Calories / serving' },
+        { type: 'answer', content: 'Less than 100', isChecked: false },
+        { type: 'answer', content: '100 - 300', isChecked: false },
+        { type: 'answer', content: 'More than 300', isChecked: false },
+        { type: 'question', content: 'Macros' },
+        { type: 'answer', content: 'High carb', isChecked: false },
+        { type: 'answer', content: 'High fat', isChecked: false },
+        { type: 'answer', content: 'High protein', isChecked: false },
+      ],
+      meals: [
+        {
+          name: 'Apple pie',
+          properties: [
+            'More than 20',
+            'Hot',
+            'Cold',
+            'Sweet',
+            'More than 300',
+            'High carb',
+            'High fat',
+          ],
+          isActive: false,
+        },
+        {
+          name: 'Burger',
+          properties: [
+            '10 - 20',
+            'Hot', 'Salty',
+            'More than 300',
+            'High fat',
+            'High protein',
+          ],
+          isActive: false,
+        },
+        {
+          name: 'Casserole',
+          properties: [
+            'More than 20',
+            'Hot',
+            'Salty',
+            'More than 300',
+            'High fat',
+          ],
+          isActive: false,
+        },
+        {
+          name: 'Cereal',
+          properties: [
+            'Less than 10',
+            'Cold',
+            'Sweet',
+            '100 - 300',
+            'High carb',
+          ],
+          isActive: false,
+        },
+        {
+          name: 'Cheesecake',
+          properties: [
+            'More than 20',
+            'Cold',
+            'Sweet',
+            'More than 300',
+            'High carb',
+            'High fat',
+          ],
+          isActive: false,
+        },
+        {
+          name: 'Chicken with rice',
+          properties: [
+            '10 - 20',
+            'Hot',
+            'Salty',
+            'Spicy',
+            'More than 300',
+            'High carb',
+            'High protein',
+          ],
+          isActive: false,
+        },
+        {
+          name: 'Drop scones',
+          properties: [
+            'Less than 10',
+            'Hot',
+            'Sweet',
+            '100 - 300',
+            'High carb',
+          ],
+          isActive: false,
+        },
+        {
+          name: 'Kebab',
+          properties: [
+            'More than 20',
+            'Hot',
+            'Salty',
+            'Spicy',
+            'More than 300',
+            'High fat',
+            'High protein',
+          ],
+          isActive: false,
+        },
+        {
+          name: 'Lasagna bolognese',
+          properties: [
+            'More than 20',
+            'Hot',
+            'Salty',
+            'More than 300',
+            'High carb',
+            'High fat',
+          ],
+          isActive: false,
+        },
+        {
+          name: 'Letcho',
+          properties: [
+            '10 - 20',
+            'Hot',
+            'Salty',
+            'Spicy',
+            '100 - 300',
+            'High fat',
+          ],
+          isActive: false,
+        },
+        {
+          name: 'Omelette',
+          properties: [
+            'Less than 10',
+            'Hot',
+            'Salty',
+            'Sweet',
+            'Less than 100',
+            'High fat',
+          ],
+          isActive: false,
+        },
+        {
+          name: 'Pancakes',
+          properties: [
+            'Less than 10',
+            'Hot',
+            'Sweet',
+            '100 - 300',
+            'High carb',
+          ],
+          isActive: false,
+        },
+        {
+          name: 'Pizza',
+          properties: [
+            'More than 20',
+            'Hot',
+            'Salty',
+            'Spicy',
+            'More than 300',
+            'High carb',
+            'High fat',
+          ],
+          isActive: false,
+        },
+        {
+          name: 'Pork chop',
+          properties: [
+            '10 - 20',
+            'Hot',
+            'Salty',
+            'More than 300',
+            'High fat',
+            'High protein',
+          ],
+          isActive: false,
+        },
+        {
+          name: 'Porridge',
+          properties: [
+            'Less than 10',
+            'Hot',
+            'Sweet',
+            'Less than 100',
+            'High carb',
+          ],
+          isActive: false,
+        },
+        {
+          name: 'Potato pancakes',
+          properties: [
+            '10 - 20',
+            'Hot',
+            'Salty',
+            '100 - 300',
+            'High carb',
+            'High fat',
+          ],
+          isActive: false,
+        },
+        {
+          name: 'Salmon penne',
+          properties: [
+            '10 - 20',
+            'Hot',
+            'Salty',
+            'More than 300',
+            'High fat',
+            'High protein',
+          ],
+          isActive: false,
+        },
+        {
+          name: 'Scrambled eggs',
+          properties: [
+            'Less than 10',
+            'Hot',
+            'Salty',
+            'Less than 100',
+            'High fat',
+            'High protein',
+          ],
+          isActive: false,
+        },
+        {
+          name: 'Spaghetti bolognese',
+          properties: [
+            'More than 20',
+            'Hot',
+            'Salty',
+            '100 - 300',
+            'High carb',
+          ],
+          isActive: false,
+        },
+        {
+          name: 'Tiramisu',
+          properties: [
+            'More than 20',
+            'Cold',
+            'Sweet',
+            'More than 300',
+            'High carb',
+            'High fat',
+          ],
+          isActive: false,
+        },
+        {
+          name: 'Toasts',
+          properties: [
+            'Less than 10',
+            'Hot',
+            'Salty',
+            'Less than 100',
+            'High carb',
+          ],
+          isActive: false,
+        },
+      ],
+      isMealChosen: false,
+      randomMeal: '',
+      disableRandomMeal: false,
+      allrecipes: false,
+      simplyrecipes: false,
+      tasteofhome: false,
+    };
+  }
+
+  toggleAnswer = (answerIndex) => {
+    const { form } = this.state;
+    const answer = form[answerIndex];
+    form.splice(answerIndex, 1, {
+      ...answer,
+      isChecked: answer.isChecked === false,
+    });
+    this.setState({ form }, this.showMeals);
+  };
+
+  showMeals = () => {
+    const { form, meals } = this.state;
+    const selectedProperties = [];
+    form.forEach((formProperty) => {
+      if (formProperty.type === 'answer' && formProperty.isChecked) {
+        selectedProperties.push(formProperty.content);
+      }
+    });
+
+    const matchingMeals = meals.map((meal) => {
+      if (selectedProperties.length) {
+        const doesMealActivate = selectedProperties.every(selectedProperty => (
+          meal.properties.includes(selectedProperty)
+        ));
+        return { ...meal, isActive: doesMealActivate };
+      }
+      return { ...meal, isActive: false };
+    });
+
+    this.setState({ meals: matchingMeals });
+  };
+
+  chooseMeal = () => {
+    const { meals } = this.state;
+    const activeMeals = meals.filter(activeMeal => activeMeal.isActive);
+    const activeMealsNum = activeMeals.length;
+    const randomMealIndex = Math.floor(Math.random() * activeMealsNum);
+    const randomMeal = activeMealsNum && activeMeals[randomMealIndex].name;
+    this.setState({ isMealChosen: true, randomMeal });
+  };
+
+  closeChosenMeal = () => {
+    this.setState({
+      isMealChosen: false,
+      randomMeal: '',
+      disableRandomMeal: false,
+      allrecipes: false,
+      simplyrecipes: false,
+      tasteofhome: false,
+    });
+  };
+
+  showLinks = () => {
+    const {
+      randomMeal,
+      disableRandomMeal,
+      allrecipes,
+      simplyrecipes,
+      tasteofhome,
+    } = this.state;
+
+    const googleSearchKey = process.env.REACT_APP_GOOGLE_SEARCH_KEY;
+    fetch(`https://www.googleapis.com/customsearch/v1?key=${googleSearchKey}=${randomMeal} recipe`)
+      .then(response => (
+        response.ok
+          ? response.json()
+          : this.setState({ disableRandomMeal: true })
+      ))
+      .then((result) => {
+        if (disableRandomMeal === false) {
+          result.items.forEach((item) => {
+            const resultLink = item.displayLink;
+            if (!allrecipes && resultLink.includes('allrecipes.com')) {
+              this.setState({ allrecipes: item.link });
+            }
+            if (!simplyrecipes && resultLink.includes('simplyrecipes.com')) {
+              this.setState({ simplyrecipes: item.link });
+            }
+            if (!tasteofhome && resultLink.includes('tasteofhome.com')) {
+              this.setState({ tasteofhome: item.link });
+            }
+            if (allrecipes || simplyrecipes || tasteofhome) {
+              this.setState({ randomMeal: '' });
+            } else {
+              this.setState({ disableRandomMeal: true });
+            }
+          });
+        }
+      });
+  };
+
+  render() {
+    const {
+      isMealChosen,
+      form,
+      meals,
+      allrecipes,
+      simplyrecipes,
+      tasteofhome,
+      randomMeal,
+      disableRandomMeal,
+    } = this.state;
+
+    return (
+      <main>
+        <header>
+          <h1 className="pageTitle">Meal Seeker</h1>
+        </header>
+        <Blur isMealChosen={isMealChosen} />
+        <div className="container">
+          <Form
+            form={form}
+            toggleAnswer={this.toggleAnswer}
+          />
+          <Meals meals={meals} />
+        </div>
+        <Footer
+          meals={meals}
+          chooseMeal={this.chooseMeal}
+        />
+        <RandomMealPopup
+          isMealChosen={isMealChosen}
+          allrecipes={allrecipes}
+          simplyrecipes={simplyrecipes}
+          tasteofhome={tasteofhome}
+          randomMeal={randomMeal}
+          disableRandomMeal={disableRandomMeal}
+          showLinks={this.showLinks}
+          closeChosenMeal={this.closeChosenMeal}
+        />
+      </main>
+    );
+  }
+}
